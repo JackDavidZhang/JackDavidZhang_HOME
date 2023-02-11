aac49ee84bb1e0e12eab9a7210a2c9b410ab07ad
JackDavidZhang <2949125232@qq.com>
2023-02-11 18:20:21 +0800

    Feature: error.html. BugFix: article font weight adjust.

--- js/error.js
+++ js/error.js
@@ -1,12 +1,42 @@
 function GetQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
+    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    const r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]);
     return null;
 }
 
 window.onload = function () {
-    errorTex = GetQueryString("errorCode");
-    tex = document.getElementById("text");
-    tex.innerText = errorTex;
+    let errorTex = GetQueryString("errorCode");
+    let errorDescribe;
+    switch (errorTex) {
+        case '404':
+            errorDescribe = "Not Found";
+            break;
+        case '400':
+            errorDescribe = "Bad Request";
+            break;
+        case '401':
+            errorDescribe = "Unauthorized";
+            break;
+        case '403':
+            errorDescribe = "Forbidden";
+            break;
+        case '409':
+            errorDescribe = "Request Timeout";
+            break;
+        case '500':
+            errorDescribe = "Internal Server Error";
+            break;
+        case '502':
+            errorDescribe = "Bad Gateway";
+            break;
+        case '504':
+            errorDescribe = "Gateway Timeout";
+            break;
+        default:
+            errorTex = "NaN";
+            errorDescribe = "UnknownError";
+    }
+    $("#text").text(errorTex);
+    $("#description").text(errorDescribe);
 }
